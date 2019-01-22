4efab449b7b47bfdea2bc48416b9e1a9cc9c7a3d
marwajomaa <marwajomaa4@gmail.com>
2019-01-22 18:36:52 +0200

    create first reducer for books list

---
+++ src/reducers/book_reducer.js
@@ -0,0 +1,13 @@
+//book reducer return an array of books and assign it to the books key in the index file
+
+ const BooksReducer = () => {
+  return [
+    {title: 'JavaScript: the good parts'},
+    {title: 'Learn react-redux'},
+    {title: 'Object-Oriented JavaScript'},
+    {title: 'How to be full stack developer'},
+    {title: 'Code yourself'},
+  ]
+}
+
+export default BooksReducer;
