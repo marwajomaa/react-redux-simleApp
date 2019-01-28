ca2289ac92eb14ac6fabf66d65317327b80b8e94
marwajomaa <marwajomaa4@gmail.com>
2019-01-28 21:23:20 +0200

    add comments

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,6 @@
+// redux have only one store then there' no need for dispacher, the store will dispacch the actions directly
+// having one store means that all state will be located in one place
+
 import { createStore } from 'redux'
 import {rootReducers} from '../reducers'
 
